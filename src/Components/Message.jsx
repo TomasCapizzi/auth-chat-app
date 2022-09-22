95a52f24595a3df05565c1859b740468befffea7
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-09-22 20:05:38 -0300

    Add Atomic components

--- src/Components/Message.jsx
+++ src/Components/Message.jsx
@@ -1,25 +1,15 @@
+import MessageAvatar from "./MessageAvatar";
+import MessageInfo from './MessageInfo';
 import React from "react";
-import {TiDelete, TiUser} from 'react-icons/ti'
+import {TiDelete} from 'react-icons/ti'
 
 export default function Message({message, user, handleDelete}){
-
 
     return (
         <div className='message-container'>
             <div className={user.uid === message.uid ? 'message send' : 'message received'}>
-                {
-                    message.photoURL ? 
-                        <img src={message.photoURL} alt="" />
-                    :
-                        <TiUser />
-                } 
-                <p>{message.text}</p>
-                <p className='date'>{message.createdAt.toDate().toDateString()}</p>
-                {  !message.photoURL && user.uid !== message.uid &&
-                        <div className='user-none'>
-                            <p>{message.email}</p>
-                        </div>
-                }
+                <MessageAvatar message={message} />
+                <MessageInfo message={message} user={user} />
             </div>
             {
                 user.uid === message.uid && <button onClick={()=> handleDelete(message.id)}><TiDelete/></button>
